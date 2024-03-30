4040370af8c5a72bad864749ee60637a5bc3b7f7
Tuanny Ruiz <tuannyruiz@gmail.com>
2024-03-30 08:13:39 -0400

    wip

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,5 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
     let low = 0;
     let high = haystack.length;
-    const m = Math.floor(low + (high - low) / 2);
+
+    do {
+        const m = Math.floor(low + (high - low) / 2);
+        const value = haystack[m];
+
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            high = m;
+        } else {
+            // value < needle
+            low = m + 1;
+        }
+    } while (low < high);
+
+    return false;
 }
