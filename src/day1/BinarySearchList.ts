815280504b908ae714b0e1fda79886063f7b1652
Tuanny Ruiz <tuannyruiz@gmail.com>
2024-03-30 06:14:23 -0400

    adding files

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,5 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+    const m = Math.floor(low + (high - low) / 2);
+}
