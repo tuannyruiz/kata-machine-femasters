9d6715d0e0932c7eff1d8de4f4b41a7e3e8fbfa6
Tuanny Ruiz <tuannyruiz@gmail.com>
2024-04-02 15:43:06 -0400

    tree traversals

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,17 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
 
+    // Pre
+    path.push(curr.value);
+    // Recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+    // Post
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
